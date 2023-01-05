ae0380a799e032d3f151b033bc4d1f19a844e638
Krasy Nova <krasy_nova@yahoo.com>
2023-01-05 16:57:54 +0100

    sandwitch menu

--- js/script.js
+++ js/script.js
@@ -1,24 +1,9 @@
 'use strict';
 
-let uls = [...document.querySelectorAll('.ul')];
-let buttonList = [...document.querySelectorAll('.anchor-btn')];
-buttonList.forEach(elem => elem.addEventListener('click', show))
+const burgerMenu = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-nav');
 
-function show() {
-
-    console.log(this);
-    for (let i = 0; i < buttonList.length; i++) {
-        if (buttonList[i] != this) {
-            uls[i].classList.add('hide-dropdown');
-            console.log(uls[i]);
-        } else {
-            // uls[i].classList.remove('dropdown-menu');
-            uls[i].classList.add('dropdown-menu');
-        }
-    }
-
-
-
-
-
-}
+burgerMenu.addEventListener('click', function() {
+    burgerMenu.classList.toggle('is-active');
+    mobileMenu.classList.toggle('is-active');
+})
