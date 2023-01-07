dc4578a27de0440ec8b3e6d097fd47a6caedc5bb
Krasy Nova <krasy_nova@yahoo.com>
2023-01-08 00:19:37 +0100

    dropdown menus

--- js/script.js
+++ js/script.js
@@ -1,32 +1,5 @@
 'use strict';
 
-let openBtn = document.querySelector("h1");
-let overlay = document.querySelector(".overlay");
-let closeBtn = document.querySelector(".fa-solid");
 
-openBtn.addEventListener("click", create);
-closeBtn.addEventListener("click", closeModal);
-
-function create() {
-  overlay.classList.remove('hidden');
-}
-function closeModal() {
-  overlay.classList.add('hidden');
-}
-
-
-
-
-// const burgerMenu = document.querySelector('.hamburger');
-// const mobileMenu = document.querySelector('.mobile-nav');
-
-// burgerMenu.addEventListener('click', function() {
-//     burgerMenu.classList.toggle('is-active');
-//     mobileMenu.classList.toggle('hidden');
-// })
-
-// let button = document.querySelector("h1");
-// let modalDiv = document.querySelector(".overlay");
-// let p = document.querySelector(".close");
 
 
